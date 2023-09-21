098883b6ad40727b14bb8d9af8c02b350e532ab1
Lilia_LuSol <maria.bochkova.2003.moon@yandex.ru>
2023-09-21 15:44:20 +0300

    update шкаф

--- test/main.dart
+++ test/main.dart
@@ -8,84 +8,177 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Шкаф цветной'),
+          title: const Text('Шкаф цветной'),
         ),
         body: Shkaf(),
       ),
     );
   }
 }
+
 class Shkaf extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.grey[200],
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                width: 185.0,
-                height: 150.0,
-                color: Colors.pink,
+      padding: const EdgeInsets.all(8),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                        ),
+                        color: Colors.pink,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '1',
+                          style: TextStyle(color: Colors.lightGreenAccent, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40),
+                        ),
+                        color: Colors.purple,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '2',
+                          style: TextStyle(color: Colors.limeAccent, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                width: 185.0,
-                height: 150.0,
-                color: Colors.purple,
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: Colors.red,
+                      child: const Center(
+                        child: Text(
+                          '3',
+                          style: TextStyle(color: Colors.black54, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: Colors.blue,
+                      child: const Center(
+                        child: Text(
+                          '4',
+                          style: TextStyle(color: Colors.orangeAccent, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                width: 185.0,
-                height: 75.0,
-                color: Colors.red,
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: Colors.orange,
+                      child: const Center(
+                        child: Text(
+                          '5',
+                          style: TextStyle(color: Colors.blueAccent, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      color: Colors.green,
+                      child: const Center(
+                        child: Text(
+                          '6',
+                          style: TextStyle(color: Colors.redAccent, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                width: 185.0,
-                height: 75.0,
-                color: Colors.blue,
+            ),
+            Expanded(
+              flex: 3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                        ),
+                        color: Colors.yellow,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '7',
+                          style: TextStyle(color: Colors.purpleAccent, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(40),
+                        ),
+                        color: Colors.brown,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '8',
+                          style: TextStyle(color: Colors.white, fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                width: 185.0,
-                height: 75.0,
-                color: Colors.orange,
-              ),
-              Container(
-                width: 185.0,
-                height: 75.0,
-                color: Colors.green,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                width: 185.0,
-                height: 150.0,
-                color: Colors.yellow,
-              ),
-              Container(
-                width: 185.0,
-                height: 150.0,
-                color: Colors.brown,
-              ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
